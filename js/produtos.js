6cded8177f9c70a237b63a051ee2448afe04aa6c
suportewebflexsites-max <suporte.webflexsites@gmail.com>
2026-01-07 19:13:29 -0300

    Update produtos.js

--- js/produtos.js
+++ js/produtos.js
@@ -1,1 +1,32 @@
+// Lista de produtos da Adega
+const produtos = [
+  {
+    id: 1,
+    nome: "Skol 269ml",
+    preco: 4.00,
+    categoria: "Cervejas",
+    imagem: "https://via.placeholder.com/80"
+  },
+  {
+    id: 2,
+    nome: "Original 269ml",
+    preco: 4.00,
+    categoria: "Cervejas",
+    imagem: "https://via.placeholder.com/80"
+  },
+  {
+    id: 3,
+    nome: "Heineken 269ml",
+    preco: 5.20,
+    categoria: "Cervejas",
+    imagem: "https://via.placeholder.com/80"
+  },
+  {
+    id: 4,
+    nome: "Amstel 269ml",
+    preco: 9.00,
+    categoria: "Cervejas",
+    imagem: "https://via.placeholder.com/80"
+  }
+];
 
